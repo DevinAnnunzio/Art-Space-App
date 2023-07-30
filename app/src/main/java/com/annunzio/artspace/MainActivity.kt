3caa4202b12b1d785fd88f40e1a7594b04ad74d1
DevinAnnunzio <annunzio.da@gmail.com>
2023-07-30 21:44:15 +0900

    Chore: Cleaned up println, leaving composable functions commented to mess with later possibly

--- app/src/main/java/com/annunzio/artspace/MainActivity.kt
+++ app/src/main/java/com/annunzio/artspace/MainActivity.kt
@@ -12,13 +12,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -41,85 +39,79 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceTheme {
-                // A surface container using the 'background' color from the theme
-
-                    var imageNum by remember {
-                        mutableStateOf(0)
-                    }
-                    //Code here
-                val modifier = Modifier
-                Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center){
-
-                    Image(painter = painterResource(id = when(imageNum){
-                        0 -> R.drawable.lemon_drink
-                        1 -> R.drawable.lemon_tree
-                        2 -> R.drawable.lemon_squeeze
-                        else -> {R.drawable.ic_launcher_foreground}
-                    }),
-                        contentDescription = "Main picture",
-                        modifier = modifier
-                            .border(BorderStroke(3.dp, Color.LightGray))
-                            .wrapContentHeight()
-                            .wrapContentWidth()
-                            .background(color = MaterialTheme.colorScheme.background))
-
-
-                    Spacer(modifier = modifier.padding(top = 80.dp))
-
-
-                    Column(modifier = modifier.border(BorderStroke(1.dp, Color.LightGray))) {
-                        Text(textAlign = TextAlign.Center,fontStyle = FontStyle.Italic, fontSize = 22.sp,text = when(imageNum){
-                            0 -> "Artwork Title: Drink of lemon"
-                            1 -> "Artwork Title: Lemon tree"
-                            2 -> "Artwork Title: Squeezie"
-                            else -> {"N/A"}
-                        })
-
-                        Text(textAlign = TextAlign.Center,fontStyle = FontStyle.Italic,fontSize = 18.sp,text = when(imageNum){
-                            0 -> "By: Sir drink 1820-1870"
-                            1 -> "By: Sir tree 1888-1911"
-                            2 -> "By: Sir Squuzie 1868-1888"
-                            else -> {"N/A"}
-                        })
-
-                    }
-
-                    Spacer(modifier = modifier.padding(top = 300.dp))
-
-
-                    Row(modifier = Modifier) {
-                        Button(onClick = {
-                            println(imageNum)
-                            println("Minus clicked")
-                            if(imageNum - 1 > -1){
-                                imageNum -= 1
-                            }
-                            println(imageNum)
-                        }) {
-                            Text(text = "Previous")
-                        }
-                        Spacer(modifier = modifier.padding(10.dp))
-
-                        Button(onClick = {
-                            println(imageNum)
-                            println("Plus clicked")
-                            if(imageNum + 1 < 3){
-                                imageNum += 1
-                            }
-                            println(imageNum)
-                        }) {
-                            Text(text = "Next")
-                        }
-
-                    }
-
-                }
-
-
-
+                MainScreenLayout()
             }
         }
+    }
+}
+
+@Composable
+fun MainScreenLayout() {
+    var imageNum by remember {
+        mutableStateOf(0)
+    }
+    //Code here
+    val modifier = Modifier
+    Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
+
+        Image(painter = painterResource(id = when(imageNum){
+            0 -> R.drawable.lemon_drink
+            1 -> R.drawable.lemon_tree
+            2 -> R.drawable.lemon_squeeze
+            else -> {R.drawable.ic_launcher_foreground}
+        }),
+            contentDescription = "Main picture",
+            modifier = modifier
+                .border(BorderStroke(3.dp, Color.LightGray))
+                .wrapContentHeight()
+                .wrapContentWidth()
+                .background(color = MaterialTheme.colorScheme.background))
+
+
+        Spacer(modifier = modifier.padding(top = 80.dp))
+
+
+        Column(modifier = modifier.border(BorderStroke(1.dp, Color.LightGray))) {
+            Text(textAlign = TextAlign.Center,fontStyle = FontStyle.Italic, fontSize = 22.sp,text = when(imageNum){
+                0 -> "Artwork Title: Drink of lemon"
+                1 -> "Artwork Title: Lemon tree"
+                2 -> "Artwork Title: Squeezie"
+                else -> {"N/A"}
+            })
+
+            Text(textAlign = TextAlign.Center,fontStyle = FontStyle.Italic,fontSize = 18.sp,text = when(imageNum){
+                0 -> "By: Sir drink 1820-1870"
+                1 -> "By: Sir tree 1888-1911"
+                2 -> "By: Sir Squuzie 1868-1888"
+                else -> {"N/A"}
+            })
+
+        }
+
+        Spacer(modifier = modifier.padding(top = 300.dp))
+
+
+        Row(modifier = Modifier) {
+            Button(onClick = {
+                if(imageNum - 1 > -1){
+                    imageNum -= 1
+                }
+            }) {
+                Text(text = "Previous")
+            }
+            Spacer(modifier = modifier.padding(10.dp))
+
+            Button(onClick = {
+                if(imageNum + 1 < 3){
+                    imageNum += 1
+                }
+            }) {
+                Text(text = "Next")
+            }
+
+        }
+
     }
 }
 
@@ -184,5 +176,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     ArtSpaceTheme {
+        MainScreenLayout()
     }
 }
